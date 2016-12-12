e3a0600b0a50de9cfc95d2fa3ff4ebea790813ac
katerynasyrotenko <katerynasyrotenko@gmail.com>
2016-12-12 16:10:11 +0200

    added scale

---
+++ js/data.js
@@ -0,0 +1,342 @@
+var map = {
+	bathroom: {
+		scale: 'small',
+		label: 'Ванная',
+		infoList: [
+			{
+				footage: 2,
+				minPrice: 39560,
+				maxPrice: 83490,
+				minTerm: 8,
+				maxTerm: 10,
+			},
+			{
+				footage: 2.5,
+				minPrice: 49450,
+				maxPrice: 104363,
+				minTerm: 8,
+				maxTerm: 10,
+			},
+			{
+				footage: 3,
+				minPrice: 51600,
+				maxPrice: 108900,
+				minTerm: 8,
+				maxTerm: 10,
+			},
+			{
+				footage: 3.5,
+				minPrice: 60200,
+				maxPrice: 114345,
+				minTerm: 9,
+				maxTerm: 11,
+			},
+			{
+				footage: 4,
+				minPrice: 68800,
+				maxPrice: 130680,
+				minTerm: 9,
+				maxTerm: 11,
+			},
+			{
+				footage: 4.5,
+				minPrice: 77400,
+				maxPrice: 147015,
+				minTerm: 9,
+				maxTerm: 11,
+			},
+			{
+				footage: 5,
+				minPrice: 79120,
+				maxPrice: 150282,
+				minTerm: 10,
+				maxTerm: 12,
+			},
+			{
+				footage: 5.5,
+				minPrice: 87032,
+				maxPrice: 165310,
+				minTerm: 10,
+				maxTerm: 12,
+			},
+			{
+				footage: 6,
+				minPrice: 94944,
+				maxPrice: 180338,
+				minTerm: 10,
+				maxTerm: 12,
+			}
+		]
+	},
+	kitchen: {
+		scale: 'small',
+		label: 'Кухня',
+		infoList: [
+			{
+				footage: 6,
+				minPrice: 26427,
+				maxPrice: 37467,
+				minTerm: 4,
+				maxTerm: 6,
+			},
+			{
+				footage: 7,
+				minPrice: 30832,
+				maxPrice: 43712,
+				minTerm: 4,
+				maxTerm: 6,
+			},
+			{
+				footage: 8,
+				minPrice: 32785,
+				maxPrice: 46481,
+				minTerm: 4,
+				maxTerm: 6,
+			},
+			{
+				footage: 9,
+				minPrice: 36883,
+				maxPrice: 52291,
+				minTerm: 5,
+				maxTerm: 7,
+			},
+			{
+				footage: 10,
+				minPrice: 39449,
+				maxPrice: 55929,
+				minTerm: 5,
+				maxTerm: 7,
+			},
+			{
+				footage: 11,
+				minPrice: 42130,
+				maxPrice: 59730,
+				minTerm: 6,
+				maxTerm: 9,
+			},
+			{
+				footage: 12,
+				minPrice: 45960,
+				maxPrice: 65160,
+				minTerm: 6,
+				maxTerm: 9,
+			}
+		]
+	},
+	room: {
+		scale: 'small',
+		label: 'Комната',
+		infoList: [
+			{
+				footage: 8,
+				minPrice: 29600,
+				maxPrice: 36000,
+				minTerm: 4,
+				maxTerm: 6,
+			},
+			{
+				footage: 9,
+				minPrice: 33300,
+				maxPrice: 40500,
+				minTerm: 4,
+				maxTerm: 6,
+			},
+			{
+				footage: 10,
+				minPrice: 37000,
+				maxPrice: 45000,
+				minTerm: 4,
+				maxTerm: 6,
+			},
+			{
+				footage: 11,
+				minPrice: 40700,
+				maxPrice: 49500,
+				minTerm: 4,
+				maxTerm: 6,
+			},
+			{
+				footage: 12,
+				minPrice: 44400,
+				maxPrice: 54000,
+				minTerm: 5,
+				maxTerm: 7,
+			},
+			{
+				footage: 13,
+				minPrice: 48100,
+				maxPrice: 58500,
+				minTerm: 5,
+				maxTerm: 7,
+			},
+			{
+				footage: 14,
+				minPrice: 51800,
+				maxPrice: 63000,
+				minTerm: 5,
+				maxTerm: 7,
+			},
+			{
+				footage: 15,
+				minPrice: 55500,
+				maxPrice: 67500,
+				minTerm: 5,
+				maxTerm: 7,
+			},
+			{
+				footage: 16,
+				minPrice: 59200,
+				maxPrice: 72000,
+				minTerm: 5,
+				maxTerm: 8,
+			},
+			{
+				footage: 17,
+				minPrice: 62900,
+				maxPrice: 76500,
+				minTerm: 5,
+				maxTerm: 8,
+			},
+			{
+				footage: 18,
+				minPrice: 66600,
+				maxPrice: 81000,
+				minTerm: 5,
+				maxTerm: 8,
+			},
+			{
+				footage: 19,
+				minPrice: 70300,
+				maxPrice: 85500,
+				minTerm: 5,
+				maxTerm: 8,
+			},
+			{
+				footage: 20,
+				minPrice: 74000,
+				maxPrice: 90000,
+				minTerm: 6,
+				maxTerm: 9,
+			},
+			{
+				footage: 21,
+				minPrice: 77700,
+				maxPrice: 94500,
+				minTerm: 6,
+				maxTerm: 9,
+			},
+			{
+				footage: 22,
+				minPrice: 81400,
+				maxPrice: 99000,
+				minTerm: 6,
+				maxTerm: 9,
+			},
+			{
+				footage: 23,
+				minPrice: 85100,
+				maxPrice: 103500,
+				minTerm: 6,
+				maxTerm: 9,
+			},
+			{
+				footage: 24,
+				minPrice: 88800,
+				maxPrice: 108000,
+				minTerm: 6,
+				maxTerm: 9,
+			}
+		]
+	},
+
+	apartment: {
+		scale: 'large',
+		label: 'Квартира',
+		infoList: [
+			{
+				minFootage: 20,
+				maxFootage: 40,
+				price: 3700,
+			},
+			{
+				minFootage: 41,
+				maxFootage: 60,
+				price: 3600,
+			},
+			{
+				minFootage: 61,
+				maxFootage: 80,
+				price: 3500,
+			},
+			{
+				minFootage: 81,
+				maxFootage: 100,
+				price: 3400,
+			},
+			{
+				minFootage: 101,
+				price: 3300,
+			}
+		]
+	},
+	house: {
+		scale: 'large',
+		label: 'Дом',
+		infoList: [
+			{
+				minFootage: 20,
+				maxFootage: 40,
+				price: 4700,
+			},
+			{
+				minFootage: 41,
+				maxFootage: 60,
+				price: 4600,
+			},
+			{
+				minFootage: 61,
+				maxFootage: 80,
+				price: 4500,
+			},
+			{
+				minFootage: 81,
+				maxFootage: 100,
+				price: 4400,
+			},
+			{
+				minFootage: 101,
+				price: 4300,
+			}
+		]
+	},
+	office: {
+		scale: 'large',
+		label: 'Офис',
+		infoList: [
+			{
+				minFootage: 20,
+				maxFootage: 40,
+				price: 2700,
+			},
+			{
+				minFootage: 41,
+				maxFootage: 60,
+				price: 2600,
+			},
+			{
+				minFootage: 61,
+				maxFootage: 80,
+				price: 2500,
+			},
+			{
+				minFootage: 81,
+				maxFootage: 100,
+				price: 2400,
+			},
+			{
+				minFootage: 101,
+				price: 2300,
+			}
+		]
+	},
+};
--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
-function onSelectChange(test) {
-	var roomType = document.querySelector('.room-type');
-	roomType.innerHTML=test.srcElement.value;
+
+function selectRoomType(roomType) {
+	var roomInfo = map[roomType];
+	var scale = roomInfo.scale;
+	var smallScale = document.querySelector ('.small-scale');
+	var largeScale = document.querySelector ('.large-scale');
+	switch (scale) {
+		case 'small': 
+			smallScale.style.display = 'block'; 
+			largeScale.style.display = 'none';
+		break;
+		case 'large': 
+			smallScale.style.display = 'none'; 
+			largeScale.style.display = 'block';
+		break;
+	}
+}
+
+function onSelectChange(event) {
+	var select = event.srcElement;
+	selectRoomType(select.value);
 };
 function onAreaChange(event) {
 	var area = document.querySelector('.area');
@@ -8,9 +26,16 @@ function onAreaChange(event) {
 };
 function onWindowLoad(){
 	var select = document.querySelector('.select');
-	var roomType = document.querySelector('.room-type');
-	roomType.innerHTML = select.value;
+	for (var name in map) {
+		var option = document.createElement ('option');	
+		option.value = name;
+		option.innerHTML = map [name].label;
+		select.appendChild (option);
+	}
 	select.addEventListener('change', onSelectChange);
+	
+	selectRoomType(select.value);
+	
 	var input = document.querySelector('.area-input');
 	input.addEventListener('change', onAreaChange);
 	input.addEventListener('keyup', onAreaChange);
