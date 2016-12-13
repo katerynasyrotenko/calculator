747b179448e390abe4bb175e74182068c84509ff
katerynasyrotenko <katerynasyrotenko@gmail.com>
2016-12-13 16:50:15 +0200

    added footage select

--- js/script.js
+++ js/script.js
@@ -8,37 +8,46 @@ function selectRoomType(roomType) {
 		case 'small': 
 			smallScale.style.display = 'block'; 
 			largeScale.style.display = 'none';
+			var select = smallScale.querySelector ('.input-area');
+			removeChildren (select);
+
+			for (var index = 0, array = roomInfo.infoList; index < array.length; index++) {
+				var item = array[index];
+				var option = document.createElement ('option');
+				option.innerHTML = item.footage;
+				option.value = index;
+				select.appendChild (option);
+			};
 		break;
+		
 		case 'large': 
-			smallScale.style.display = 'none'; 
+			smallScale.style.display = 'none';
 			largeScale.style.display = 'block';
 		break;
 	}
 }
+function removeChildren(element) {
+	while (element.hasChildNodes()) {
+		element.removeChild(element.lastChild)
+	}
+};
 
 function onSelectChange(event) {
 	var select = event.srcElement;
 	selectRoomType(select.value);
-};
-function onAreaChange(event) {
-	var area = document.querySelector('.area');
-	area.innerHTML = event.srcElement.value;
 };
 function onWindowLoad(){
 	var select = document.querySelector('.select');
 	for (var name in map) {
 		var option = document.createElement ('option');	
 		option.value = name;
-		option.innerHTML = map [name].label;
+		var room = map[name];
+		option.innerHTML = room.label;
 		select.appendChild (option);
 	}
 	select.addEventListener('change', onSelectChange);
 	
 	selectRoomType(select.value);
-	
-	var input = document.querySelector('.area-input');
-	input.addEventListener('change', onAreaChange);
-	input.addEventListener('keyup', onAreaChange);
 }
 window.addEventListener("load", onWindowLoad);
 console.log('immediately');
