f740d0718289e739a8e668ed5f431df0d72088e6
katerynasyrotenko <katerynasyrotenko@gmail.com>
2016-12-20 13:29:19 +0200

    added js code for large scale room types

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 function selectRoomType(roomType) {
-	var roomInfo = map[roomType];
-	var scale = roomInfo.scale;
+	var roomInfo = map[roomType]; //map.roomType - не правильно, потому что это равносильно map['roomType'] - нам надо взять то, что находится внутри roomtype, а не сам roomtype
+	var scale = roomInfo.scale; //roomInfo['scale']
 	var smallScale = document.querySelector ('.small-scale');
 	var largeScale = document.querySelector ('.large-scale');
 	switch (scale) {
@@ -18,14 +18,40 @@ function selectRoomType(roomType) {
 				option.value = index;
 				select.appendChild (option);
 			};
+			showSmallScale(array[parseInt(select.value)], smallScale);
 		break;
-		
+
 		case 'large': 
 			smallScale.style.display = 'none';
 			largeScale.style.display = 'block';
+			showLargeScale(roomInfo.infoList[0], largeScale);
 		break;
 	}
 }
+function showLargeScale (object, template) {
+	var inputArea = template.querySelector('.input-area');
+	inputArea.value = object.minFootage;
+	var area = template.querySelector ('.area');
+	area.innerHTML = object.minFootage;
+	var unitPrice = template.querySelector('.unit-price');
+	unitPrice.innerHTML = object.price;
+	var totalPrice = template.querySelector('.total-price');
+	totalPrice.innerHTML = inputArea.value * object.price;
+}
+
+function showSmallScale(object, template) {
+	var area = template.querySelector('.area');
+	area.innerHTML = object.footage;
+	var minPrice = template.querySelector('.min-price');
+	minPrice.innerHTML = object.minPrice;
+	var maxPrice = template.querySelector('.max-price');
+	maxPrice.innerHTML = object.maxPrice;
+	var minTerm = template.querySelector('.min-term');
+	minTerm.innerHTML = object.minTerm;
+	var maxTerm = template.querySelector('.max-term');
+	maxTerm.innerHTML = object.maxTerm;
+}
+
 function removeChildren(element) {
 	while (element.hasChildNodes()) {
 		element.removeChild(element.lastChild)
@@ -36,6 +62,29 @@ function onSelectChange(event) {
 	var select = event.srcElement;
 	selectRoomType(select.value);
 };
+
+function onSmallScaleAreaChange(event) {
+	var select = event.srcElement;
+	var roomTypeSelect = document.querySelector('.select');
+	var array = map[roomTypeSelect.value].infoList;
+	showSmallScale(array[select.value], document.querySelector('.small-scale'));
+}
+function onLargeScaleAreaChange(event) {
+	var roomTypeSelect = document.querySelector('.select');
+	var array = map[roomTypeSelect.value].infoList;
+	var inputValue = event.srcElement.value; //это будет input
+	for (var i = 0; i < array.length; i++) {
+		var item = array[i];
+		if (item.maxFootage == undefined) {
+			break;
+		}
+		if (inputValue >= item.minFootage && inputValue <= item.maxFootage) {
+			break;
+		}
+	}
+	showLargeScale(item, document.querySelector ('.large-scale'));
+}
+
 function onWindowLoad(){
 	var select = document.querySelector('.select');
 	for (var name in map) {
@@ -46,6 +95,8 @@ function onWindowLoad(){
 		select.appendChild (option);
 	}
 	select.addEventListener('change', onSelectChange);
+	document.querySelector('.small-scale .input-area').addEventListener('change', onSmallScaleAreaChange);
+	document.querySelector('.large-scale .input-area').addEventListener('change', onLargeScaleAreaChange);
 	
 	selectRoomType(select.value);
 }
